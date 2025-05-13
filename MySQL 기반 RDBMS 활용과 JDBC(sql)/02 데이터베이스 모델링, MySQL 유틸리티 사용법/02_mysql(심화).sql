76b27acfb42f6d8e7f399a64341d07afd0df3a55
Saeun Park <ajsklaoao@naver.com>
2025-05-13 15:08:46 +0900

    Delete MySQL 기반 RDBMS 활용과 JDBC(sql)/02 데이터베이스 모델링, MySQL 유틸리티 사용법/02_mysql(심화).sql

--- MySQL 기반 RDBMS 활용과 JDBC(sql)/02 데이터베이스 모델링, MySQL 유틸리티 사용법/02_mysql(심화).sql
+++
@@ -1,8 +0,0 @@
--- tbl_board의 데이터를 모두 출력
-select * from shopdb.tbl_board;
-
--- tbl_board의 데이터를 출력 (단, no, title, writer만 출력)
-select no, title, writer from shopdb.tbl_board;
-
--- tbl_board의 데이터 중 특정 작성자 한 명의 글만 출력
-select * from shopdb.tbl_board where writer="박사은";
