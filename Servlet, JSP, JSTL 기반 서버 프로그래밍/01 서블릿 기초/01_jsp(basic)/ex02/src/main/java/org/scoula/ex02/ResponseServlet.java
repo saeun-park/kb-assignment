b43c66af43d8c06f7cc45f8b7b96e4aa15e26937
Saeun Park <ajsklaoao@naver.com>
2025-06-04 16:26:23 +0900

    Add files via upload

---
+++ Servlet, JSP, JSTL 기반 서버 프로그래밍/01 서블릿 기초/01_jsp(basic)/ex02/src/main/java/org/scoula/ex02/ResponseServlet.java
@@ -0,0 +1,27 @@
+package org.scoula.ex02;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/response")
+public class ResponseServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        // MIME 타입 설정
+        resp.setContentType("text/html;charset=UTF-8");
+
+        // 자바 I/O
+        PrintWriter out = resp.getWriter();
+
+        // html 작성 및 출력
+        out.println("<html><body>");
+        out.println("ResponseServlet 요청 성공");
+        out.println("</body></html>");
+    }
+}
